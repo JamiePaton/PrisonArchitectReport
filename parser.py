ef3438ecf89ab5a71696a39dd7a232427dc186cc
JamiePaton <JamiePaton@users.noreply.github.com>
2015-07-30 00:42:31 +0100

    game dictionary
    creates dictionary of game data

--- parser.py
+++ parser.py
@@ -5,7 +5,7 @@ Created on Wed Jul 08 19:27:44 2015
 @author: Jamie
 """
 TITLE = 'Prison Architect Savegame Parser'
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 AUTHOR = 'Jamie E Paton'
 TEST = 0
 
@@ -16,6 +16,7 @@ import unittest
 import hypothesis as hs
 import os
 import json
+import jsonobject
 import pyparsing
 
 
@@ -46,16 +47,29 @@ def read_file(savegame):
         data = prison.read()
     return data
 
-class SaveGame(object):
+class SaveGame(jsonobject.JSONObject):
     """
         Represents a Prison Architect savegame
         d = {key: value for (key, value) in iterable}
     """
     def __init__(self, filename):
         self.data = SaveGame.read_file(filename).split('\n')
-#        self.settings = [" ".join(line.split()).split() for line in self.data[1:22]]
-        self.settings = {entry[0]: entry[1] for entry in
-                        [" ".join(line.split()).split() for line in self.data[1:22]]}
+        self.game = {'Settings': {entry[0]: entry[1] for entry in
+                                 [" ".join(line.split()).split() for line in self.data[1:22]]}}
+        start = None
+        finish = None
+        for i, line in enumerate(self.data):
+            if line[:5] == "BEGIN" and start is None:
+                if "END" in line:
+                    continue
+                start = i
+            elif line[:5] == "END" and finish is None:
+                finish = i + 1
+                self.game[self.data[start][5:].strip()] = "kk"
+                self.game[self.data[start][5:].strip()] = pyparsing.nestedExpr('BEGIN', 
+                          'END').parseString(' '.join(self.data[start:finish])).asList()
+                start = None
+                finish = None
 
     @staticmethod
     def read_file(filename):
@@ -64,16 +78,11 @@ class SaveGame(object):
         return data
 
 def main(args):
-#    prison_data = read_file('testing1.prison').split('\n')
-#    body = '\n'.join(prison_data[22:])
-#    bodylist = pyparsing.nestedExpr('BEGIN', 'END').parseString(body).asList()
-#    print 'done'
     import pprint
 #    
-#    pprint.pprint(bodylist)
 
     s = SaveGame('testing1.prison')
-    pprint.pprint(s.settings)
+    print s.game.keys()
 
 class Testing(unittest.TestCase):
     """
